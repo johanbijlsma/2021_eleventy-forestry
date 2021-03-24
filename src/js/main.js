a3cbb34fab40b9162cf15010c3a4e87945f1a82b
Johan Bijlsma <johan.bijlsma24@gmail.com>
2021-03-24 17:17:20 +0100

    feature: added dark-mode

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// const body = document.querySelector('body');
+// const toggleBtn = document.querySelector('#toggleBtn');
+
+// toggleBtn.addEventListener('click', (e) => {
+//     body.classList.toggle('dark');
+// })
